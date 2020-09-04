021bb0e62bc34e0a13920fb180b5111019192ead
Josh McMillan <josh@joshmcmillan.co.uk>
2020-09-04 07:11:38 +0100

    Remove percent encoding of local files
    This is causing URLs to be double escaped, which results in local paths
    that are invalid because the app has a space in its name.

--- ios/RNTrackPlayer/Models/MediaURL.swift
+++ ios/RNTrackPlayer/Models/MediaURL.swift
@@ -12,16 +12,15 @@ struct MediaURL {
     let value: URL
     let isLocal: Bool
     private let originalObject: Any
-    
+
     init?(object: Any?) {
         guard let object = object else { return nil }
         originalObject = object
-        
+
         if let localObject = object as? [String: Any] {
             let uri = localObject["uri"] as! String
             isLocal = uri.contains("http") ? false : true
-            let encodedURI = uri.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
-            value = URL(string: encodedURI.replacingOccurrences(of: "file://", with: ""))!
+            value = URL(string: uri.replacingOccurrences(of: "file://", with: ""))!
         } else {
             let url = object as! String
             isLocal = url.lowercased().hasPrefix("file://")
